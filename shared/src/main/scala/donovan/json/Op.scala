79a58416466ad136eaa2f0dbb147b8a3a9f6dc1e
Aaron Pritzlaff <aaron.pritzlaff@wejo.com>
2019-03-13 12:50:25 +0000

    2.11 support

--- shared/src/main/scala/donovan/json/Op.scala
+++ shared/src/main/scala/donovan/json/Op.scala
@@ -2,6 +2,10 @@ package donovan.json
 import io.circe.Decoder.Result
 import io.circe._
 
+/** Represents the operator which should be used when evaluating values within the same json document
+  *
+  * @param name the operator name
+  */
 sealed abstract class Op(val name: String) {
 
   def eval(lhs: Json, rhs: Json): Boolean
@@ -12,12 +16,15 @@ object Op {
   implicit object Format extends Encoder[Op] with Decoder[Op] {
     override def apply(a: Op): Json = Json.fromString(a.name)
     override def apply(c: HCursor): Result[Op] = {
-      c.as[String].flatMap { key =>
-        val upperKey = key.toUpperCase
-        Values.find(_.name == upperKey) match {
-          case Some(found) => Right(found)
-          case None        => Left(DecodingFailure(Values.mkString("Expected one of '", "', '", s"' but got '${key}'"), c.history))
-        }
+      // flatMap is only available from 2.12, and we still support 2.11. Good 'ol Spark...
+      c.as[String] match {
+        case Right(key) =>
+          val upperKey = key.toUpperCase
+          Values.find(_.name == upperKey) match {
+            case Some(found) => Right(found)
+            case None        => Left(DecodingFailure(Values.mkString("Expected one of '", "', '", s"' but got '${key}'"), c.history))
+          }
+        case Left(err) => Left(err)
       }
     }
   }
@@ -29,7 +36,7 @@ object Op {
     GT,
     GTE,
     Before,
-    After,
+    After
   )
 
   case object Equals extends Op("EQ") {
@@ -39,15 +46,18 @@ object Op {
   }
   case object Before extends Op("BEFORE") {
     override def eval(lhs: Json, rhs: Json): Boolean = {
-      rhs.as[String].exists { referenceTime =>
-        donovan.json.Before(referenceTime).matches(lhs)
+      // .exists on Either is only available from 1.12
+      rhs.as[String] match {
+        case Left(_)              => false
+        case Right(referenceTime) => donovan.json.Before(referenceTime).matches(lhs)
       }
     }
   }
   case object After extends Op("AFTER") {
     override def eval(lhs: Json, rhs: Json): Boolean = {
-      rhs.as[String].exists { referenceTime =>
-        donovan.json.After(referenceTime).matches(lhs)
+      rhs.as[String] match {
+        case Right(referenceTime) => donovan.json.After(referenceTime).matches(lhs)
+        case Left(_) => false
       }
     }
   }
